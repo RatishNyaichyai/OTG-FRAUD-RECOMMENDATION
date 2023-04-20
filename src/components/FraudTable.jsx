ed86ae431eb2b1eda21f849f96385cab453d8cea
sampatdhakal1999 <sampatdhakal@gmail.com>
2023-04-20 17:59:51 +0545

    Moved searchbar and dropdown to table component

--- src/components/FraudTable.jsx
+++ src/components/FraudTable.jsx
@@ -3,9 +3,38 @@ import { Table } from "antd";
 import csvData from "../data/final_demo_explanationimproved - final_demo_df.csv";
 import Papa from "papaparse";
 import "./styles/FraudTable.css";
+import LocationDropdownMenu from "./LocationDropdownMenu";
+import SearchBar from "./SearchBar";
 
 const FraudTable = () => {
   const [data, setData] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("ALL");
+  const [dataTypeArray, setDataTypeArray] = useState([]);
+
+  const handleLocationChange = (option) => {
+    setSelectedLocation(option.label);
+  };
+
+  const onSearch = (searchValue) => {
+    let filteredData = dataTypeArray;
+    if (searchValue !== "") {
+      filteredData = dataTypeArray.filter((row) => {
+        const values = Object.values(row).map((value) =>
+          value.toString().toLowerCase()
+        );
+        return values.some((value) => value.includes(searchValue.toLowerCase()));
+      });
+    }
+    setData(filteredData);
+  };
+
+  const Location = [
+    { value: "ALL", label: "ALL" },
+    { value: "SCC", label: "SCC" },
+    { value: "BUR", label: "BUR" },
+    { value: "SFO", label: "SFO" },
+  ];
+
   const columns = [
     {
       title: "Date",
@@ -28,6 +57,7 @@ const FraudTable = () => {
       key: "suspicious",
     },
   ];
+
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(csvData);
@@ -38,15 +68,25 @@ const FraudTable = () => {
         TransactionID: row["TransactionID"],
         TransactionAmt: row["TransactionAmt"],
         suspicious: row["suspicious"],
+        location: row["Location"],
       }));
-      setData(dataTypeArray);
+      setDataTypeArray(dataTypeArray);
+      const filteredData = selectedLocation === "ALL"
+        ? dataTypeArray
+        : dataTypeArray.filter((row) => row.location === selectedLocation);
+      setData(filteredData);
     }
     fetchData();
-  }, []);
+  }, [selectedLocation]);
 
   return (
-    <div className="table">
-      <Table columns={columns} dataSource={data} />
+    <div>
+      <SearchBar onSearch={onSearch} />
+      <LocationDropdownMenu options={Location} onSelection={handleLocationChange}
+        selectedLocation={selectedLocation} />
+      <div className="table">
+        <Table columns={columns} dataSource={data} />
+      </div>
     </div>
   );
 };
